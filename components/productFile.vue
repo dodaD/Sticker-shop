c193c7be2893cf2e6644791382bee5b7b1c093d3
diana <mafka487@gmail.com>
2024-05-04 14:28:41 -0700

    created catalog page; trying to pass an image from parentComp

---
+++ components/productFile.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="product-wrapper">
+    <img v-if="img" :src="img" />
+    <img v-if="img" src="/assets/img/kidd.png" />
+
+    <div class="descpription-wrapper">
+      <div class="price-wrapper">
+        <span class="title"> {{ title }} </span>
+        <span class="price"> {{ price }} </span>
+      </div>
+      <button class="add-to-cart">Add to cart</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  img?: string
+  title: string
+  price: string // WARNING:  This should be a number
+}>()
+</script>
+
+<style>
+.product-wrapper {
+  width: 25%;
+  margin-left: 10px;
+  display: flex;
+}
+
+.descpription-wrapper {
+  display: flex;
+}
+
+.price-wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+.title {
+  font-size: 20px;
+}
+
+.price {
+  font-size: 16px;
+}
+
+.add-to-cart {
+  height: 100%;
+}
+</style>
+
---
+++ pages/catalog.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="wrapper">
+    <productFile title="Product 1" price=0.15 />
+    <productFile title="Product 2" price=0.15 />
+    <productFile title="Product 3" price=0.15 />
+    <productFile title="Product 4" price=0.15 img="assets/img/kidd.png" />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+
