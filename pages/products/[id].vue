ed3734e570d5f98149b8a910e3a628f391f65c23
dodaD <104919713+dodaD@users.noreply.github.com>
2024-07-15 18:19:49 -0700

    update

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,23 +1,35 @@
 <template>
-  <!--<div class="flex xl:max-w-[1460px] xl:min-w-[1160px] md:max-w-[580px] lg:min-w-[910px]" v-if="product !== undefined">
-    <div v-for="picture in picturesForThisProduct.pictures">
-      <img :src="'/images/' + picture" class="w-[260px] h-[370px]" />
+  <div
+    class="flex w-fit mx-auto xl:max-w-[1460px] xl:min-w-[1160px] md:max-w-[580px] lg:min-w-[910px] lg:flex-row flex-col"
+    v-if="store.response.products !== undefined">
+
+    <div class="flex flex-col xl:flex-row">
+      <div
+        class="flex-row xl:flex-col overflow-scroll hidden lg:flex xl:max-h-[740px] h-fit translate-y-[740px] xl:translate-y-[0px]">
+        <img :src="'/images/' + picture" v-for="picture in picturesForThisProduct.pictures"
+          @click="changeActiveSlide(picture)" class="h-[100px] w-fit rounded-lg mr-2"
+          :class="{ border: activePicture == picture, 'border-sky-500': activePicture === picture, }" />
+      </div>
+
+      <swiper :modules="modules" class="w-[600px] h-[740px] !px-[40px] lg:translate-y-[-100px] xl:translate-y-[0px]"
+        :slides-per-view="1" navigation @swiper="onSwiper" :scrollbar="{ draggable: true }"
+        @slideChange="onSlideChange">
+        <swiper-slide v-for="picture in picturesForThisProduct.pictures">
+          <img :src="'/images/' + picture" class="w-[100%] h-[100%] rounded-lg"
+            :class="{ hidden: activePicture !== picture }" />
+        </swiper-slide>
+      </swiper>
     </div>
 
-    <img :src="'/images/' + product.imgURL" class="w-[260px] h-[370px]" </div> /> -->
-  <swiper :modules="modules" :slides-per-view="1" :space-between="50" navigation @swiper="onSwiper"
-    :scrollbar="{ draggable: true }" class="max-w-[540px] h-[740px]" @slideChange="onSlideChange">
-    <swiper-slide v-for="picture in picturesForThisProduct.pictures">
-      <img :src="'/images/' + picture" class="scale-[2] translate-x-[50%] translate-y-[50%]" />
-    </swiper-slide>
-  </swiper>
-  <div class="flex column">
-    <img :src="'/images/' + picture" v-for="picture in picturesForThisProduct.pictures"
-      @click="changeActiveSlide(picture)"
-      :class="{ border: activePicture == picture, 'border-sky-500': activePicture == picture, }" />
+    <div>
+      <h2 class="my-4">{{ product.title }}</h2>
+      <span class="pb-8 border-b-2 border-indigo-500 w-[100%] block">${{ product.price }}0</span>
+      <div class="flex mt-6 flex-col">
+        <span class="mb-2">Options: Standart</span>
+        <img :src="'/images/' + product.imgURL" class="h-[90px] w-fit rounded-lg" />
+      </div>
+    </div>
   </div>
-
-
 </template>
 
 <script setup>
