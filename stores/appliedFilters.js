deca6f05ea7b2f3aef2db709f0cfc840186f0fdc
Diana Doda <ddoda916@sd58.bc.ca>
2024-09-12 12:40:09 -0700

    fixed filters store for future filters

--- stores/appliedFilters.js
+++ stores/appliedFilters.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 
 export const useAppliedFiltersStore = defineStore("appliedFilters", () => {
-  const filters = ref([]);
+  const filters = ref({"types":[], "sizes":[], "themes": []});
 
   function $reset() {
     filters.value = [];
