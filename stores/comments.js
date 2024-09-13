7aeca996967afa440b38eb26e9df8909d2575510
Diana Doda <ddoda916@sd58.bc.ca>
2024-09-13 11:43:12 -0700

    fake json

--- stores/comments.js
+++ stores/comments.js
@@ -2,28 +2,88 @@ import { defineStore } from "pinia";
 
 export const useCommentsStore = defineStore("comments", () => {
   async function getCommentsForProduct(product_id) {
-    const response = await fetch('http://localhost/api/comments/' + product_id);
-    const json = await response.json();
-    return json;
+    return {
+      "comments": {
+        "current_page": 1,
+        "data": [
+          {
+            "id": 1,
+            "name": "User 1",
+            "text": "loved it!!! <3",
+            "stars": 5,
+            "created_at": "2024-07-26 00:30:05"
+          },
+          {
+            "id": 2,
+            "name": "User 2",
+            "text": "slay",
+            "stars": 5,
+            "created_at": "2024-07-26 00:30:05"
+          }
+        ],
+        "first_page_url": "http://example.com/api/comments?product_id=1&page=1",
+        "from": 1,
+        "last_page": 1,
+        "last_page_url": "http://example.com/api/comments?product_id=1&page=1",
+        "next_page_url": null,
+        "path": "http://example.com/api/comments",
+        "per_page": 2,
+        "prev_page_url": null,
+        "to": 2,
+        "total": 2
+      }
+    };
   }
 
   async function getStarsForProduct(product_id) {
-    const response = await fetch('http://localhost/api/comments/get_stars/' + product_id);
-    const json = await response.json();
-    return json;
+    return {
+      "stars": 4.0,
+      "amount_of_comments": 4,
+    };
   }
 
   async function getStarStatisticsForProduct(product_id) {
-    const response = await fetch('http://localhost/api/comments/get_stars_statistics/' + product_id);
-    const json = await response.json();
-    return json;
+    return {
+      "1": 1,
+      "2": 0,
+      "3": 2,
+      "4": 0,
+      "5": 4
+    }
   }
 
   async function getCommentsWithSpecificStars(product_id, stars) {
-    const response = await fetch('http://localhost/api/comments/get_comments_with_stars/' + product_id + '/' + stars);
-    const json = await response.json();
-    return json;
-  }
+    return {
+      "comments": {
+        "current_page": 1,
+        "data": [
+          {
+            "id": 1,
+            "name": "User 1",
+            "text": "loved it!!! <3",
+            "stars": 5,
+            "created_at": "2024-07-26 00:30:05"
+          },
+          {
+            "id": 6,
+            "name": "User 2",
+            "text": "slay",
+            "stars": 5,
+            "created_at": "2024-07-26 00:30:05"
+          }
+        ],
+        "first_page_url": "http://example.com/api/comments?product_id=1&stars=5&page=1",
+        "from": 1,
+        "last_page": 2,
+        "last_page_url": "http://example.com/api/comments?product_id=1&stars=5&page=2",
+        "next_page_url": "http://example.com/api/comments?product_id=1&stars=5&page=2",
+        "path": "http://example.com/api/comments",
+        "per_page": 2,
+        "prev_page_url": null,
+        "to": 2,
+        "total": 3
+      }
+    }  }
 
   return { getCommentsForProduct, getStarsForProduct, getStarStatisticsForProduct, getCommentsWithSpecificStars };
 });
--- stores/products.js
+++ stores/products.js
@@ -4,21 +4,258 @@ export const useProductsStore = defineStore("products", () => {
   let response = {};
 
   async function getProducts() {
-    const response = await fetch('http://localhost/api/products');
-    const json = await response.json();
-    return json;
+    return {
+      "products": [
+        {
+          "id": 1,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Abedo Portrait Sticker, Genshin",
+          "imgURL": "albedo.jpg",
+          "theme": "Genshin Impact",
+          "size": "small",
+          "type": "sticker",
+          "price": 0.5,
+          "description": "Wonderfull sticker of glorified alchemic Albedo"
+        },
+        {
+          "id": 2,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Beidou 3/4 Sticker, Genshin",
+          "imgURL": "beidou.jpg",
+          "theme": "Genshin Impact",
+          "size": "large",
+          "type": "sticker",
+          "price": 1.5,
+          "description": ""
+        },
+        {
+          "id": 3,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Gan Yun Portrait Sticker, Genshin",
+          "imgURL": "gan_yun.jpg",
+          "theme": "Genshin Impact",
+          "size": "small",
+          "type": "sticker",
+          "price": 0.5,
+          "description": ""
+        },
+        {
+          "id": 4,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Kazuha Sticker, Genshin",
+          "imgURL": "kazuha.jpg",
+          "theme": "Genshin Impact",
+          "size": "medium",
+          "type": "sticker",
+          "price": 1,
+          "description": ""
+        },
+        {
+          "id": 5,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Tighnari Half Body Sticker, Genshin",
+          "imgURL": "tignari.jpg",
+          "theme": "Genshin Impact",
+          "size": "medium",
+          "type": "sticker",
+          "price": 1,
+          "description": ""
+        },
+        {
+          "id": 6,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Venti with Flowers Sticker, Genshin",
+          "imgURL": "venti.jpg",
+          "theme": "Genshin Impact",
+          "size": "large",
+          "type": "sticker",
+          "price": 1.5,
+          "description": ""
+        },
+        {
+          "id": 7,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Xiao Sticker, Genshin",
+          "imgURL": "xiao.jpg",
+          "theme": "Genshin Impact",
+          "size": "small",
+          "type": "sticker",
+          "price": 0.5,
+          "description": ""
+        },
+        {
+          "id": 8,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Kidd Iconic Postcard, Soul Eater",
+          "imgURL": "kidd.png",
+          "theme": "Soul Eater",
+          "size": "large",
+          "type": "postcard",
+          "price": 2,
+          "description": "Oh come on, you DO need it. And now just order it"
+        }
+      ]
+    }; 
   }
 
   async function getAdditionalPicturesForProduct(product_id) {
-    const response = await fetch('http://localhost/api/additional_pictures/' + product_id);
-    const json = await response.json();
-    return json;
+    return {
+      "optional_pictures": [
+        {
+          "id": 15,
+          "created_at": null,
+          "updated_at": null,
+          "imgURL": "albedo.jpg",
+          "product_id": 1,
+          "optinal": 1
+        },
+        {
+          "id": 16,
+          "created_at": null,
+          "updated_at": null,
+          "imgURL": "kazuha.jpg",
+          "product_id": 1,
+          "optinal": 1
+        }
+      ],
+      "additional_pictures": [
+        {
+          "id": 15,
+          "created_at": null,
+          "updated_at": null,
+          "imgURL": "albedo.jpg",
+          "product_id": 1,
+          "optinal": 1
+        },
+        {
+          "id": 16,
+          "created_at": null,
+          "updated_at": null,
+          "imgURL": "kazuha.jpg",
+          "product_id": 1,
+          "optinal": 1
+        },
+        {
+          "id": 17,
+          "created_at": null,
+          "updated_at": null,
+          "imgURL": "xiao.jpg",
+          "product_id": 1,
+          "optinal": 0
+        }
+      ]
+};
   }
 
   async function getYouMightLikeProducts(product_id) {
-    const response = await fetch('http://localhost/api/products/get_you_might_like/' + product_id);
-    const json = await response.json();
-    return json;
+    return {
+      "products": [
+        {
+          "id": 1,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Abedo Portrait Sticker, Genshin",
+          "imgURL": "albedo.jpg",
+          "theme": "Genshin Impact",
+          "size": "small",
+          "type": "sticker",
+          "price": 0.5,
+          "description": "Wonderfull sticker of glorified alchemic Albedo"
+        },
+        {
+          "id": 2,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Beidou 3/4 Sticker, Genshin",
+          "imgURL": "beidou.jpg",
+          "theme": "Genshin Impact",
+          "size": "large",
+          "type": "sticker",
+          "price": 1.5,
+          "description": ""
+        },
+        {
+          "id": 3,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Gan Yun Portrait Sticker, Genshin",
+          "imgURL": "gan_yun.jpg",
+          "theme": "Genshin Impact",
+          "size": "small",
+          "type": "sticker",
+          "price": 0.5,
+          "description": ""
+        },
+        {
+          "id": 4,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Kazuha Sticker, Genshin",
+          "imgURL": "kazuha.jpg",
+          "theme": "Genshin Impact",
+          "size": "medium",
+          "type": "sticker",
+          "price": 1,
+          "description": ""
+        },
+        {
+          "id": 5,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Tighnari Half Body Sticker, Genshin",
+          "imgURL": "tignari.jpg",
+          "theme": "Genshin Impact",
+          "size": "medium",
+          "type": "sticker",
+          "price": 1,
+          "description": ""
+        },
+        {
+          "id": 6,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Venti with Flowers Sticker, Genshin",
+          "imgURL": "venti.jpg",
+          "theme": "Genshin Impact",
+          "size": "large",
+          "type": "sticker",
+          "price": 1.5,
+          "description": ""
+        },
+        {
+          "id": 7,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Xiao Sticker, Genshin",
+          "imgURL": "xiao.jpg",
+          "theme": "Genshin Impact",
+          "size": "small",
+          "type": "sticker",
+          "price": 0.5,
+          "description": ""
+        },
+        {
+          "id": 8,
+          "created_at": null,
+          "updated_at": null,
+          "title": "Kidd Iconic Postcard, Soul Eater",
+          "imgURL": "kidd.png",
+          "theme": "Soul Eater",
+          "size": "large",
+          "type": "postcard",
+          "price": 2,
+          "description": "Oh come on, you DO need it. And now just order it"
+        }
+      ]
+};
   }
 
   return { response, getProducts, getAdditionalPicturesForProduct, getYouMightLikeProducts };
